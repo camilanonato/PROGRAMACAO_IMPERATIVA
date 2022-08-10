2495e2114d3affc2990306f42364e5646e6a5c28
Camila Nonato <105988072+camilanonato@users.noreply.github.com>
2022-08-09 22:00:36 -0300

    Add files via upload

---
+++ Mesa_4/nutri.js
@@ -0,0 +1,11 @@
+var nome = 'José';
+var sobrenome = 'da Silva';
+var idade = 27;
+var peso = 83.5;
+var altura = 1.70;
+var plano = true;
+var imc = null;
+
+imc = (peso) / (altura * altura);
+
+console.log(nome + " " +sobrenome + " tem " + idade + " anos e seu índice de massa corporal é de: " + imc);
