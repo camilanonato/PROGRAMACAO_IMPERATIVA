04678ff6deb4fc4511b79bb76ce3473c4dd8de47
Camila Nonato <105988072+camilanonato@users.noreply.github.com>
2022-08-15 22:21:06 -0300

    Add files via upload

---
+++ Mesa_7/mesa_7.js
@@ -0,0 +1,18 @@
+//Exercício 1
+//1 false
+//2 true
+//3 false
+//4 true
+//5 false
+//6 true
+//7 true
+//8 false
+//9 false
+//10 a. false
+//10 b. true
+//10 c. true
+
+//Exercício 2
+//1 true
+//2 false
+//3 false
